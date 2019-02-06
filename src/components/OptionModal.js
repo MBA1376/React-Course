feb235c107f4cea2c75271aacdc547e7ed8c19f2
unknown <Asus@example.com>
2019-02-06 23:50:01 +0330

    complete Indecision App

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,13 +4,15 @@ import Modal from 'react-modal';
 const OptionModal = (props) => {
 	return (
 		<Modal
+			className="modal"
 			isOpen={!!props.selectedOption} 
 			onRequestClose={props.handleClearSelectedOption}
 			contentLabel="Selected Option"
+			closeTimeoutMS={400}
 		>
-			<h3>some text</h3>
-			{props.selectedOption && <p>{props.selectedOption}</p>}
-			<button onClick={props.handleClearSelectedOption}>Okay</button>
+			<h3 className="modal__title">some text</h3>
+			{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+			<button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
 		</Modal>
 	)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,19 @@ module.exports = {
 			loader : 'babel-loader' ,
 			test : /\.js$/ ,
 			exclude : /node_modules/
+		} ,
+		{
+			test : /\.s?(c|a)ss$/,
+			 use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
 		}]
 	} ,
 	devtool : 'cheap-module-eval-source-map' ,
 	devServer : {
 		contentBase : path.join(__dirname , 'public')
-	}
+	} 
+	
 }
